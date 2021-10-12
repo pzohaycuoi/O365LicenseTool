cccfe102e62a86d5c7352709c06c15d5bf1e04f8
Yahaga <59613584+pzohaycuoi@users.noreply.github.com>
2021-10-12 22:34:13 +0700

    Delete filebrowser.ps1

--- filebrowser.ps1
+++
@@ -1,8 +0,0 @@
-do {
-  Add-Type -AssemblyName System.Windows.Forms
-  $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
-  $FileBrowser.filter = "Csv (*.csv)| *.csv"
-  [void]$FileBrowser.ShowDialog()
-  $filePath = $FileBrowser.FileName
-} until ("" -ne $filePath)
-
